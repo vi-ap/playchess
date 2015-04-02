9fba1b8194dbc18d3621ef84e61c0aaae1fe6441
vi-ap <aparna.23@outlook.com>
2015-04-01 23:22:15 -0700

    Moving the express prject out of the chess subdirectory into the root directory. Replacing server.js with simpler example.

--- server.js
+++ server.js
@@ -1,112 +1,72 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+    console.log(req);
     console.log("Request received");
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello from node server!');
+    res.end('<img src="http://minionslovebananas.com/images/gallery/preview/Chiquita-DM2-minion-dave-bananas.jpg?w=420&h=356"/>');
 
 }).listen(process.env.PORT || 8080);
 
 console.log("Server running");
 
 //var express = require('express');
+//var path = require('path');
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var bodyParser = require('body-parser');
+
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+
 //var app = express();
 
-//app.get('/', function (request, response) {
-//    response.send('Hello from server!');
+//// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+//// uncomment after placing your favicon in /public
+////app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', routes);
+//app.use('/users', users);
+
+//// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//  var err = new Error('Not Found');
+//  err.status = 404;
+//  next(err);
 //});
 
-//var server = app.listen(3000, function () {
-//    console.log('Server listening');
+//// error handlers
+
+//// development error handler
+//// will print stacktrace
+//if (app.get('env') === 'development') {
+//  app.use(function(err, req, res, next) {
+//    res.status(err.status || 500);
+//    res.render('error', {
+//      message: err.message,
+//      error: err
+//    });
+//  });
+//}
+
+//// production error handler
+//// no stacktraces leaked to user
+//app.use(function(err, req, res, next) {
+//  res.status(err.status || 500);
+//  res.render('error', {
+//    message: err.message,
+//    error: {}
+//  });
 //});
 
-//#!/usr/bin/env node
 
-///**
-// * Module dependencies.
-// */
-
-//var app = require('./app');
-//var debug = require('debug')('chess:server');
-//var http = require('http');
-
-///**
-// * Get port from environment and store in Express.
-// */
-
-//var port = normalizePort(process.env.PORT || '3000');
-//app.set('port', port);
-
-///**
-// * Create HTTP server.
-// */
-
-//var server = http.createServer(app);
-
-///**
-// * Listen on provided port, on all network interfaces.
-// */
-
-//server.listen(port);
-//server.on('error', onError);
-//server.on('listening', onListening);
-
-///**
-// * Normalize a port into a number, string, or false.
-// */
-
-//function normalizePort(val) {
-//  var port = parseInt(val, 10);
-
-//  if (isNaN(port)) {
-//    // named pipe
-//    return val;
-//  }
-
-//  if (port >= 0) {
-//    // port number
-//    return port;
-//  }
-
-//  return false;
-//}
-
-///**
-// * Event listener for HTTP server "error" event.
-// */
-
-//function onError(error) {
-//  if (error.syscall !== 'listen') {
-//    throw error;
-//  }
-
-//  var bind = typeof port === 'string'
-//    ? 'Pipe ' + port
-//    : 'Port ' + port;
-
-//  // handle specific listen errors with friendly messages
-//  switch (error.code) {
-//    case 'EACCES':
-//      console.error(bind + ' requires elevated privileges');
-//      process.exit(1);
-//      break;
-//    case 'EADDRINUSE':
-//      console.error(bind + ' is already in use');
-//      process.exit(1);
-//      break;
-//    default:
-//      throw error;
-//  }
-//}
-
-///**
-// * Event listener for HTTP server "listening" event.
-// */
-
-//function onListening() {
-//  var addr = server.address();
-//  var bind = typeof addr === 'string'
-//    ? 'pipe ' + addr
-//    : 'port ' + addr.port;
-//  debug('Listening on ' + bind);
-//}
+//module.exports = app;
