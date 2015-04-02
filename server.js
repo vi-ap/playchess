bc2c8243fdb455efce7014dc30183bbea45e5af9
vi-ap <aparna.23@outlook.com>
2015-04-01 23:52:49 -0700

    Uncommenting express code from server.js

--- server.js
+++ server.js
@@ -1,72 +1,72 @@
-var http = require('http');
+//var http = require('http');
 
-http.createServer(function (req, res) {
-    console.log(req);
-    console.log("Request received");
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<img src="http://minionslovebananas.com/images/gallery/preview/Chiquita-DM2-minion-dave-bananas.jpg?w=420&h=356"/>');
+//http.createServer(function (req, res) {
+//    console.log(req);
+//    console.log("Request received");
+//    res.writeHead(200, { 'Content-Type': 'text/html' });
+//    res.end('<img src="http://minionslovebananas.com/images/gallery/preview/Chiquita-DM2-minion-dave-bananas.jpg?w=420&h=356"/>');
 
-}).listen(process.env.PORT || 8080);
+//}).listen(process.env.PORT || 8080);
 
-console.log("Server running");
+//console.log("Server running");
 
-//var express = require('express');
-//var path = require('path');
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-//var app = express();
+var app = express();
 
-//// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-//// uncomment after placing your favicon in /public
-////app.use(favicon(__dirname + '/public/favicon.ico'));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', routes);
-//app.use('/users', users);
+app.use('/', routes);
+app.use('/users', users);
 
-//// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//  var err = new Error('Not Found');
-//  err.status = 404;
-//  next(err);
-//});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-//// error handlers
+// error handlers
 
-//// development error handler
-//// will print stacktrace
-//if (app.get('env') === 'development') {
-//  app.use(function(err, req, res, next) {
-//    res.status(err.status || 500);
-//    res.render('error', {
-//      message: err.message,
-//      error: err
-//    });
-//  });
-//}
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
-//// production error handler
-//// no stacktraces leaked to user
-//app.use(function(err, req, res, next) {
-//  res.status(err.status || 500);
-//  res.render('error', {
-//    message: err.message,
-//    error: {}
-//  });
-//});
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 
-//module.exports = app;
+module.exports = app;
