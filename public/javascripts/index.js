1be2c23620c43466acf176ccd744a9abee22a3a1
vi-ap <aparna.23@outlook.com>
2015-04-06 14:10:47 -0700

    Adding client javascript file. Adding drag and drop functionality

---
+++ public/javascripts/index.js
@@ -0,0 +1,12 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    ev.target.appendChild(document.getElementById(ev.dataTransfer.getData("text")));
+}
