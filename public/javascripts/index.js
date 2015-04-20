e1170ac8821be2e01297ef7cdc6ba8dfd6951000
vi-ap <aparna.23@outlook.com>
2015-04-20 11:24:17 -0700

    Adding unicode chess piece character, adding jquery's drag and drop widget, commenting out some test code.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,18 +1,20 @@
-function allowDrop(ev) {
-    ev.preventDefault();
-}
+//function allowDrop(ev) {
+//    ev.preventDefault();
+//}
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
+//function drag(ev) {
+//    ev.dataTransfer.setData("text", ev.target.id);
+//}
 
-function drop(ev) {
-    ev.preventDefault();
-    ev.target.appendChild(document.getElementById(ev.dataTransfer.getData("text")));
+//function drop(ev) {
+//    ev.preventDefault();
+//    ev.target.appendChild(document.getElementById(ev.dataTransfer.getData("text")));
     
-    if (document.getElementById("SoundOption").checked)
-    {
-        var snd = new Audio("/audio/horse.mp3"); // buffers automatically when created
-        snd.play();
-    }
-}
+//    if (document.getElementById("SoundOption").checked)
+//    {
+//        var snd = new Audio("/audio/horse.mp3"); // buffers automatically when created
+//        snd.play();
+//    }
+//}
+
+$('.chess-piece').draggable();
